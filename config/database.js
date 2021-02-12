6c9b47ba28a25aa8424e7891f51e4b4764060816
Benoît Valle <benoit.valleea@hotmail.fr>
2021-02-12 19:31:40 +0100

    postgres credentials

--- config/database.js
+++ config/database.js
@@ -5,10 +5,13 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: 'ec2-54-220-35-19.eu-west-1.compute.amazonaws.com',
+        port:5432,
+        database: 'debl63mhk7ff8g',
+        password:'ab5510b4857a22935cc78b9dfe7518053a4dbec20f8bcc5cf61aab4e0dd3fc74'
       },
       options: {
-        useNullAsDefault: true,
+        rejectUnauthorized: false
       },
     },
   },
